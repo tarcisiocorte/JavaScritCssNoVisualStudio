f64ec4b2a1c3de90cbbd7759882e487211509828
tarcisiocorte <tarcisiocorte@gmail.com>
2018-03-01 10:29:22 -0300

    Atualizando dados

--- javascript-avancado/js/main.js
+++ javascript-avancado/js/main.js
@@ -16,7 +16,7 @@ function getTotal(list){
 function setList(list){
     var table = '<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>';
     for(var key in list){
-        table += '<tr><td>'+ formatDesc(list[key].desc) +'</td><td>'+ list[key].amount +'</td><td>'+ formatValue(list[key].value) +'</td><td>Edit | Delete</td></tr>';
+        table += '<tr><td>'+ formatDesc(list[key].desc) +'</td><td>'+ list[key].amount +'</td><td>'+ formatValue(list[key].value) +'</td><td><button class="btn btn-default" onclick="setUpdate('+key+');" >Edit</button> Delete</td></tr>';
     }
     table += '</tbody>';
     document.getElementById("listTable").innerHTML = table;
@@ -42,8 +42,42 @@ function addData(){
 
     list.unshift({"desc":desc , "amount":amount ,"value":value });
     setList(list);
+}
+
+function setUpdate(id){
+    var obj = list[id];
+    document.getElementById("desc").value = obj.desc;
+    document.getElementById("amount").value = obj.amount;
+    document.getElementById("value").value = obj.value;
+    document.getElementById("btnUpdate").style.display = "inline-block";
+    document.getElementById("btnAdd").style.display = "none";
+
+    document.getElementById("inputIDUpdate").innerHTML = '<input id="idUpdate" type="hidden" value="'+id+'">';
+}
+
+function resetForm(){
+    document.getElementById("desc").value = "";
+    document.getElementById("amount").value = "";
+    document.getElementById("value").value = "";
+    document.getElementById("btnUpdate").style.display = "none";
+    document.getElementById("btnAdd").style.display = "inline-block";
+
+    document.getElementById("inputIDUpdate").innerHTML = "";
+}
+
+function updateData(){
+    var id = document.getElementById("idUpdate").value;
+    var desc = document.getElementById("desc").value;
+    var amount = document.getElementById("amount").value;
+    var value = document.getElementById("value").value;
+
+    list[id] = {"desc":desc, "amount": amount, "value":value };
+    resetForm();
+    setList(list);
 
 }
+
+
 
 setList(list);
 console.log(getTotal(list));
